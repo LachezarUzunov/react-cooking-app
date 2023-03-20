8da58dc59eba81e7d30b729845de44f38fad9e96
lachezaruzunov <uzunov_89_lacho@abv.bg>
2023-03-20 09:51:45 +0200

    refactoring

--- frontend/src/components/recipes/SmallRecipeItem.js
+++ frontend/src/components/recipes/SmallRecipeItem.js
@@ -1,20 +1,20 @@
 import React from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import classes from "./SmallRecipeItem.module.css";
-import Button from "../layout/Button";
+
 import Line from "../layout/Line";
 
 const SmallRecipeItem = ({ recipe }) => {
   const recipeExcerpt = recipe.preparation.substring(0, 150) + "...";
   const user = recipe.user;
-
+  console.log(recipe);
   return (
     <React.Fragment>
       <div className={classes.recipesList}>
         <img
           className={classes.smallPic}
           src={require(`../../../public/uploads/${recipe.photos}`)}
-          alt="chicken-breast"
+          alt={recipe.title}
         ></img>
         <div>
           <h2 className={classes.mobi__title}>{recipe.title}</h2>
